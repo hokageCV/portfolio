917c3f3be0d2450223d186ac2eca5dbea7ad222f
hokageCV <chaitanyavaru@gmail.com>
2022-12-22 17:25:38 +0530

    fix pdf no file error

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
         <footer className="footer footer-center p-4 bg-snow-3 dark:bg-night-2 text-night-3 dark:text-snow-3">
             <div>
                 <a 
-                    href="/files/2210.pdf"
+                    href="/assets/files/2210.pdf"
                     className='p-1 border rounded font-medium  bg-frost-3 text-night-4 hover:bg-frost-2'
                     download="ChaitanyaVaruResume"
                     >
