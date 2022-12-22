64d99e1cedb8629df46d9a7a7a643fac5370ee8c
hokageCV <chaitanyavaru@gmail.com>
2022-12-22 17:46:27 +0530

    add resume

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import resume from '/assets/files/2210.pdf'
 
 export default function Footer(){
     return (
         <footer className="footer footer-center p-4 bg-snow-3 dark:bg-night-2 text-night-3 dark:text-snow-3">
             <div>
                 <a 
-                    href="/assets/files/2210.pdf"
+                    href={resume}
                     className='p-1 border rounded font-medium  bg-frost-3 text-night-4 hover:bg-frost-2'
                     download="ChaitanyaVaruResume"
+                    target="_blank"
                     >
                     Resume
                 </a>
