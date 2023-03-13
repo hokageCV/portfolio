6d2010b542c81c8d8fd5ccc28bf4fb7ede4499cb
hokageCV <chaitanyavaru@gmail.com>
2023-03-13 19:49:52 +0530

    add vanta.net

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from './components/sections/Navbar'
-import About from './components/sections/About'
-import Skills from './components/sections/Skills'
-import Projects from './components/sections/Projects'
-import Contact from './components/sections/Contact'
-import Footer from './components/sections/Footer'
-
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/sections/Navbar";
+import About from "./components/sections/About";
+import Skills from "./components/sections/Skills";
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
 
 export default function App() {
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState("dark");
 
-  const toggleTheme =  () => setTheme(theme === 'dark' ? 'light' : 'dark') ;
+  const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
 
   // theme toggle
   useEffect(() => {
-		if (theme === 'dark') { document.documentElement.classList.add('dark'); } 
-    else { document.documentElement.classList.remove('dark'); }
-	}, [theme]);
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
 
   return (
     <div className="App bg-snow-1 dark:bg-night-1">
@@ -27,6 +29,5 @@ export default function App() {
       <Contact />
       <Footer />
     </div>
-  )
+  );
 }
-
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,34 +1,48 @@
-import React from 'react';
-import Socials from '../utilities/Socials';
-import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+import React, { useState, useRef, useEffect } from "react";
+import Socials from "../utilities/Socials";
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import NET from "vanta/dist/vanta.net.min";
 
-export default function About(){
-    return (
-        <div id='about' className="hero min-h-screen">
-            <div className="hero-content text-center">
-                <div className="max-w-md text-night-3">
-                    <h1 className="text-5xl font-bold dark:text-snow-4">Hi, I am Chaitanya</h1>
-                    <p className="py-6 dark:text-snow-2">
-                        Full stack developer. I Help Companies Enhance Their Web Development Process with Efficient and Organized Solutions.
-                    </p>
+export default function About() {
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
-                    <div className="btn-group">
-                        <Socials 
-                            link="https://github.com/hokageCV" 
-                            Icon={FaGithub}
-                        />
-                        <Socials 
-                            link="https://www.linkedin.com/in/chaitanya-varu/" 
-                            Icon={FaLinkedinIn}
-                        />
-                        <Socials 
-                            link="https://twitter.com/ChaitanyaVaru" 
-                            Icon={FaTwitter}
-                        />
-                    </div>
+  return (
+    <div id="about" ref={myRef} className="hero min-h-screen ">
+      <div className="hero-content text-center z-10">
+        <div className="max-w-md ">
+          <h1 className="text-5xl font-bold text-snow-4">Hi, I am Chaitanya</h1>
+          <p className="py-6 text-snow-2">
+            Full stack developer. I Help Companies Enhance Their Web Development Process with
+            Efficient and Organized Solutions.
+          </p>
 
-                </div>
-            </div>
+          <div className="btn-group">
+            <Socials link="https://github.com/hokageCV" Icon={FaGithub} />
+            <Socials link="https://www.linkedin.com/in/chaitanya-varu/" Icon={FaLinkedinIn} />
+            <Socials link="https://twitter.com/ChaitanyaVaru" Icon={FaTwitter} />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
