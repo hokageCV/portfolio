9fac4e652cd353a5c93a2b5c7f085a57e4bf12cb
hokageCV <chaitanyavaru@gmail.com>
2023-05-01 20:47:39 +0530

    add blog

--- src/components/utilities/NavMenu.jsx
+++ src/components/utilities/NavMenu.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import { Link } from 'react-scroll'
-import { Sun, Moon } from './Pokemon';
+import React from "react";
+import { Link } from "react-scroll";
+import { Sun, Moon } from "./Pokemon";
 
-export default function NavMenu({theme, toggleTheme}){
-    return(
-        <div className="flex-none">
-            <ul className="menu menu-horizontal px-1 py-0">
-                <li><Link activeClass="active" to="about" smooth={true} className="active:bg-frost-3">About</Link></li>
-                <li><Link to="skills" smooth={true} className="active:bg-frost-3 py-0">Skills</Link></li>
-                <li><Link to="projects" smooth={true} className="active:bg-frost-3 py-0">Projects</Link></li>
-                <li><Link to="contact" smooth={true} className="active:bg-frost-3 py-0">Contact</Link></li>
-                <li>
-                    <button 
-                        type='submit'
-                        onClick={toggleTheme}
-                        className=" right-20 top-4 text-lg p-1 ml-2 rounded-md bg-night-1 dark:bg-snow-1 dark:text-night-1"
-                    >
-                        {theme==='dark'? Sun : Moon}
-                    </button>
-                </li>
-            </ul>
-        </div>
-    )
+export default function NavMenu({ theme, toggleTheme }) {
+  return (
+    <div className="flex-none">
+      <ul className="menu menu-horizontal px-1 py-0">
+        <li>
+          <Link activeClass="active" to="about" smooth={true} className="active:bg-frost-3">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} className="active:bg-frost-3 py-0">
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" smooth={true} className="active:bg-frost-3 py-0">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} className="active:bg-frost-3 py-0">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <a
+            href="https://chaitanya-varu-blog.netlify.app"
+            target="_blank"
+            className="active:bg-frost-3 py-0"
+          >
+            Blog
+          </a>
+        </li>
+        <li>
+          <button
+            type="submit"
+            onClick={toggleTheme}
+            className=" right-20 top-4 text-lg p-1 ml-2 rounded-md bg-night-1 dark:bg-snow-1 dark:text-night-1"
+          >
+            {theme === "dark" ? Sun : Moon}
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
