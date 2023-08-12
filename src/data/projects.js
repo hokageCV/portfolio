02d6e6c2c8dab5d5f08b74215e4b5d4d7db854e1
hokageCV <chaitanyavaru@gmail.com>
2023-08-12 18:58:25 +0530

    update project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,25 @@
-import share_naam from '/assets/project_imgs/share_naam.gif'
-import memory_game from '/assets/project_imgs/memory_game.png'
+import memory_game from '/assets/project_imgs/memory_game.png';
+import share_naam from '/assets/project_imgs/share_naam.gif';
 // import etch_a_sketch from '/assets/project_imgs/etch_a_sketch.png'
-import weather_app from '/assets/project_imgs/weather_app.png'
-
 
 export default [
     {
-        name: "Share Naam", img: share_naam, 
-        githubLink: 'https://github.com/hokageCV/share-naam', 
-        liveLink: 'https://github.com/hokageCV/share-naam'
-    },   
+        name: 'Share Naam',
+        img: share_naam,
+        githubLink: 'https://github.com/hokageCV/share-naam',
+        liveLink: 'https://github.com/hokageCV/share-naam',
+    },
     {
-        name: "Memory Game", img: memory_game, 
-        githubLink: 'https://github.com/hokageCV/memory-game', 
-        liveLink: 'https://hokagecv.github.io/memory-game/'
-    },   
-    // {
-    //     name: "Etch A Sketch", img: etch_a_sketch, 
-    //     githubLink: 'https://github.com/hokageCV/etchAsketch', 
-    //     liveLink: 'https://hokagecv.github.io/etchAsketch/'
-    // },   
+        name: 'Memory Game',
+        img: memory_game,
+        githubLink: 'https://github.com/hokageCV/memory-game',
+        liveLink: 'https://hokagecv.github.io/memory-game/',
+    },
+
     {
-        name: "Weather App", img: weather_app, 
-        githubLink: 'https://github.com/hokageCV/Weather-App', 
-        liveLink: 'https://hokagecv.github.io/Weather-App/'
-    },     
-]
+        name: 'Clip Database Manager',
+        img: 'https://res.cloudinary.com/dmtacem5p/image/upload/v1691219867/github/clip_manager_konark.png',
+        githubLink: 'https://github.com/hokageCV/konark-automobiles',
+        liveLink: 'https://konark-automobiles.vercel.app',
+    },
+];
